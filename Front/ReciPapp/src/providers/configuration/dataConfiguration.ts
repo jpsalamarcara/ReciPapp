0021c162cb2773489ca1d68dbf7fe4a148666206
odalvarezpe <o.a.alvarez@accenture.com>
2018-08-05 14:00:15 -0500

    Update

--- Front/ReciPapp/src/providers/configuration/dataConfiguration.ts
+++ Front/ReciPapp/src/providers/configuration/dataConfiguration.ts
@@ -6,25 +6,29 @@ export const C_LIST_MATERIAL: Array<Material> = [
         id: 1,
         description: "Plástico",
         unit: "Kg",
-        valueMiddle: 200
+        valueMiddle: 200,
+        image: "plastic.png"
     },
     {
         id: 2,
         description: "Vidrios",
         unit: "Kg",
-        valueMiddle: 100
+        valueMiddle: 100,
+        image: "glass.png"
     },
     {
         id: 3,
         description: "Cartón",
         unit: "Kg",
-        valueMiddle: 150
+        valueMiddle: 150,
+        image: "paper.png"
     },
     {
         id: 4,
         description: "Orgánicos",
         unit: "Kg",
-        valueMiddle: 250
+        valueMiddle: 250,
+        image: "organic.png"
     }
 ];
 
