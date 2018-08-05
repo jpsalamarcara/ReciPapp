7a5ef524506e47f150f19069bdb9b9c7c16670eb
adsanz12 <adsanz12@gmail.com>
2018-08-04 20:17:16 -0500

    FK
    Now it works

--- Database/recipapp.sql
+++ Database/recipapp.sql
@@ -6,7 +6,7 @@
 create table core.user(
 	id int primary key not null,
 	name text not null,
-	category references core.user_category(id)
+	category int references core.user_category(id)
 );
 
 create table core.product(
@@ -17,16 +17,16 @@ create table core.product(
 create table core.cart(
 	id int primary key not null,
 	creation_date timestamp default current_timestamp,
-	generator references core.user(id),
+	generator int references core.user(id),
 	generator_date timestamp,
-	collector references core.user(id),
+	collector int references core.user(id),
 	collector_date timestamp,
-	center references core.user(id),
+	center int references core.user(id),
 	center_date timestamp
 );
 
 create table core.product_x_cart(
 	id int primary key not null,
-	product_id references core.product(id),
-	cart_id references core.cart(id),
+	product_id int references core.product(id),
+	cart_id int references core.cart(id)
 );
